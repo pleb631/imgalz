c7c66ac6e2ca2ac1a63079243dc5486461dab76b
pleb631 <895825191@qq.com>
2025-06-16 23:27:52 +0800

    refactor: refactor ByteTrack class
    - Simplified the `track` function for cleaner structure
    - Modified `_tracker_update` to add class ID output and improve logic

--- imgalz/models/tracker/byte_track/bytetracker.py
+++ imgalz/models/tracker/byte_track/bytetracker.py
@@ -1,66 +1,57 @@
 import numpy as np
 
 from .tracker.byte_tracker import BYTETracker
-
-
-
-def tlwh_to_xyxy(tlwh):
-    """ " Convert tlwh to xyxy"""
-    x1 = tlwh[0]
-    y1 = tlwh[1]
-    x2 = tlwh[2] + x1
-    y2 = tlwh[3] + y1
-    return [x1, y1, x2, y2]
-
+from imgalz import ltwh2xyxy
 
 
 class ByteTrack:
     def __init__(
-        self, min_box_area: int = 10, aspect_ratio_thresh: float = 3.0) -> None:
+        self, min_box_area: int = 10, aspect_ratio_thresh: float = 3.0,*args,**kwargs
+    ) -> None:
 
         self.min_box_area = min_box_area
         self.aspect_ratio_thresh = aspect_ratio_thresh
         self.min_box_area = min_box_area
 
-        self.tracker = BYTETracker(frame_rate=30)
+        self.tracker = BYTETracker(*args,**kwargs)
 
-    def track(self, bgr_img,dets_xyxy) -> tuple:
-
-        image = bgr_img
-        image_info = {"width": image.shape[0], "height": image.shape[1]}
-        class_ids = []
-        ids = []
-        bboxes_xyxy = []
-        scores = []
+    def track(self, bgr_img, dets_xyxy) -> tuple:
 
         if isinstance(dets_xyxy, np.ndarray) and len(dets_xyxy) > 0:
-            bboxes_xyxy, ids, scores = self._tracker_update(
+            bboxes_xyxy, ids, scores, cls = self._tracker_update(
                 dets_xyxy,
-                image_info,
+                bgr_img,
             )
         track_info = {
             "bbox_ltrb": bboxes_xyxy,
             "ids": ids,
             "scores": scores,
-            "class_ids": class_ids,
+            "class_ids": cls,
         }
         return track_info
 
-    def _tracker_update(self, dets: np.ndarray, image_info: dict):
+    def _tracker_update(self, dets: np.ndarray, bgr_img: np.ndarray):
         online_targets = []
-        class_id = 0
+
         if dets is not None:
             online_targets = self.tracker.update(dets)
 
         online_xyxys = []
         online_ids = []
         online_scores = []
+        online_clss = []
         for online_target in online_targets:
             tlwh = online_target.tlwh
             track_id = online_target.track_id
             vertical = tlwh[2] / tlwh[3] > self.aspect_ratio_thresh
             if tlwh[2] * tlwh[3] > self.min_box_area and not vertical:
-                online_xyxys.append(tlwh_to_xyxy(tlwh))
+                online_xyxys.append(ltwh2xyxy(tlwh))
                 online_ids.append(track_id)
                 online_scores.append(online_target.score)
-        return np.array(online_xyxys), np.array(online_ids), np.array(online_scores)
+                online_clss.append(online_target.cls)
+        return (
+            np.array(online_xyxys),
+            np.array(online_ids),
+            np.array(online_scores),
+            np.array(online_clss),
+        )
